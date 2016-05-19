02d9e0f04eb594c62b5bb6718266c9f78169c4b0
Diego Narducci <diego@css4html.com.br>
2016-05-19 15:31:06 -0300

    removendo codigos desnecessários

--- src/js/actions/LayoutActions.js
+++ src/js/actions/LayoutActions.js
@@ -3,19 +3,7 @@ import dispatcher from "../dispatcher";
 //CONSTANTS
 import * as Constants from "../constants/Constants";
 
-//AXIOS
-import * as axios from 'axios';
-
 export function toogleLoading() {
-  axios.get('http://localhost:3000/narducci')
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (response) {
-      console.log(response);
-    });
-
-
   dispatcher.dispatch({
     type: Constants.TOOGLE_LOADING,
   });
